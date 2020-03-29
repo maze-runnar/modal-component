ae66ae56ce04867b58eacdbb00ccbe83707a513e
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-03-29 14:00:38 +0530

    Update socialshare-modal.ts

--- modal/src/socialshare-modal.ts
+++ modal/src/socialshare-modal.ts
@@ -19,7 +19,9 @@ export default class ScrubberBar extends LitElement  {
 
 static get properties() {
   return {
-    title: { type: String } //adding dynamic title via props
+    title: { type: String }, //adding dynamic title via props
+    embeded: { type: String },
+    sharedText: {type:String},
   };
 }
 
@@ -55,6 +57,7 @@ static get styles() {
       border: 1px solid #888;
       width: 40%;
       border-radius: 8px;
+
       }
 
       .close:hover{
@@ -77,14 +80,19 @@ static get styles() {
       opacity: 0.7;
       }
 
-
+      .embeded-text {
+        color:white;
+        background-color: #727375;
+        border-radius:5px;
+        font-family:arial;
+        font-size:15px
+      }
     `;
   }
 
 
 constructor() {
-  super();
-  this.show = false; 
+  super(); 
 }
 
 handleCancel(){
@@ -94,9 +102,7 @@ handleCancel(){
 
 	render() {
     return html`
-      <div>
-        
-      
+      <div>  
       <div style ="text-align:center">
     		<button  @click = "${() =>  this.show = true }" class="share">
           <icon-hamburger></icon-hamburger>
@@ -104,7 +110,7 @@ handleCancel(){
       </div>
         ${this.show ?
          html`
-          <div class="modal">
+          <div class="modal" >
           <div class="modal-content">
             <div style= "float:right">
               <button @click = "${this.handleCancel}" class="close">
@@ -119,14 +125,14 @@ handleCancel(){
             <button style = "border:none;background-color:white" class ="share">
               <image src="https://www.pngitem.com/pimgs/m/250-2509449_twiter-twitter-icon-transparent-hd-png-download.png" width="63px">
             </button>
-            <button style = "border:none;background-color:white" class="share">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank">
               <image src="https://cdn.freebiesupply.com/logos/large/2x/facebook-icon-logo-png-transparent.png" width="50px">
-            </button>
+            </a>
             <button style = "border:none;background-color:white" class="share">
               <image src="https://toppng.com/uploads/preview/reddit-logo-reddit-icon-115628658968pe8utyxjt.png" width="50px">
             </button>
              <button style = "border:none;background-color:white" class="share">
-              <image src="https://lh3.googleusercontent.com/proxy/UFyLsp_HuhM6SpHX17mS-_VoVeuUVrh7ZANnA4Yg-526I2Tb7j_ybJLT3exu176TmZz5wlLwrbAJzFnC9Ju0Cv3qCA" width="50px">
+              <image src="https://www.sharethis.com/wp-content/uploads/2017/05/Tumblr.png" width="50px">
             </button>
             <button style = "border:none;background-color:white" class="share">
               <image src="https://i.ya-webdesign.com/images/pinterest-logo-png-transparent-background-3.png" width="50px">
@@ -134,6 +140,24 @@ handleCancel(){
             <button style = "border:none;background-color:white" class="share">
               <image src="https://pluspng.com/img-png/email-icon-png-download-icons-logos-emojis-email-icons-2400.png" width="50px">
             </button>
+            <br/>
+             ${this.embeded == "true" ? html `
+               <div>
+                 <div style = "margin-top: 50px">
+                  <span style = "font-weight : bold; font-family:arial">EMBED </span>
+                  <br/>
+                  <textarea rows="4" cols="57" class="embeded-text"><iframe src="https://archive.org/embed/${this.sharedText}" width="500" height="140" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+                  </textarea>
+                 </div>
+                
+                 <div style = "margin-top: 10px">
+                  <span style="font-weight:bold;font-family:arial">EMBED (for wordpress.com hosted blogs and archive.org item <description> tags) </span><br/>
+                  <textarea  rows="4" cols="57" class="embeded-text">[archiveorg ${this.sharedText} width=640 height=140 frameborder=0 webkitallowfullscreen=true mozallowfullscreen=true]
+                  </textarea>
+                 </div>
+               </div>
+             `:
+             html``}
 
 
           </div>
